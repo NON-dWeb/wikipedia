9c5b77de20ce3a13f14431499244033847c236e4
NON-dWeb <noemienino94@gmail.com>
2021-03-23 10:22:04 -0400

    ajout fichiers de mon wiki

---
+++ php.sql
@@ -0,0 +1,17 @@
+CREATE TABLE forum_sujets (
+	id int(6) NOT NULL auto_increment,
+	auteur VARCHAR(30) NOT NULL,
+	titre text NOT NULL,
+	date_derniere_reponse datetime NOT NULL default '0000-00-00 00:00:00',
+	PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+CREATE TABLE forum_reponses (
+	id int(6) NOT NULL auto_increment,
+	auteur VARCHAR(30) NOT NULL,
+	message text NOT NULL,
+	date_reponse datetime NOT NULL default '0000-00-00 00:00:00',
+	correspondance_sujet int(6) NOT NULL,
+	PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
